9d76d1cd8571b7b5f72b817b79f9363845b442ba
Hélio Fernandes <104803451+HelioAnacronista@users.noreply.github.com>
2022-10-28 17:17:05 -0300

    header

--- studyangular/src/app/app.module.ts
+++ studyangular/src/app/app.module.ts
@@ -6,18 +6,27 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 //Material
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { HeaderComponent } from './components/views/header/header.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
 
 //Componentes
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule
     
   ],
   providers: [],
