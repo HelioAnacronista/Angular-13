c5b23a516ba65168b45d592f9553e9eb11f707fe
Hélio Fernandes <104803451+HelioAnacronista@users.noreply.github.com>
2022-11-02 07:53:28 -0300

    add router e home component

--- studyangular/src/app/app-routing.module.ts
+++ studyangular/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavComponent } from './components/template/nav/nav.component';
+import { ToListCrudComponent } from './components/template/to-list-crud/to-list-crud.component';
+import { HomeComponent } from './views/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component : HomeComponent},
+  {path: 'tolists', component : ToListCrudComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
